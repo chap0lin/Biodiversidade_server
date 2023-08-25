b2c4bfa38d4c1a35a05f7b04fd9550a77ccfc361
chap0lin <carlosrochacaar@gmail.com>
2023-08-24 21:08:56 -0300

    using fs

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors')
-
+const fs = require("fs")
 const routes = require('./routes')
 
 const QuestionsController = require('./Controllers/QuestionsController')
